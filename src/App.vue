bc42de0c76fc086baba1f9ad1fe4a02ad783f3d8
Gabriel Soares <gabrielrs.job@gmail.com>
2023-05-06 00:16:37 -0300

    style: responsividade aplicada

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="w-full flex justify-center p-2">
-        <p class="text-2xl font-bold">JordanShoes</p>
-    </div>
-    <Banner/>
-    <div class="flex flex-col gap-2 w-full items-center mt-4">
-        <h2 class="font-bold text-[32px]">Destaques</h2>
-        <p class="text-[20px] text-[#474747]">Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado. </p>
-    </div>
+    <header>
+        <div class="w-full flex justify-center p-2">
+            <p class="text-2xl font-bold">JordanShoes</p>
+        </div>
+        <Banner/>
+    </header>
+    <section>
+        <div class="flex flex-col gap-2 w-full items-center mt-4">
+            <h2 class="font-bold text-[22px] sm:text-[32px]">Destaques</h2>
+            <p class="text-[18px] sm:[text-20px] text-center text-[#474747]">Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado. </p>
+        </div>
+    </section>
     <div class="flex justify-center">
-        <div class="grid grid-cols-12 gap-4 mt-8 p-4">
-            <div v-for="path in 8" :key="path" class="col-span-3">
+        <div class="flex flex-col sm:grid sm:grid-cols-12 gap-4 mt-8 p-4">
+            <div v-for="path in 8" :key="path" class="sm:col-span-3">
                 <ShoesCard :path-image="`/src/assets/imgs/shoe${path}.svg`"/>
             </div>
         </div>
@@ -21,6 +25,7 @@
 import Banner from './components/Banner.vue';
 import ShoesCard from './components/ShoesCard.vue';
 import Footer from './components/Footer.vue';
+
 const imagesPath = [
     {
         path: '/src/assets/imgs/shoe1.png'
--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="relative h-[370px]">
+    <div class="relative h-[370px] w-[100vw]">
         <div class="absolute inset-0 bg-jordan-banner brightness-50 backdrop-filter backdrop-brightness-50"></div>
-        <div class="flex gap-4 flex-col w-[500px] h-full justify-center ml-[200px]">
-            <h1 class="text-white font-md text-[44px] relative z-10">A melhor loja de Jordan</h1>
-            <p class="text-white font-md text-[22px] relative z-10">O tênis Jordan é fruto de uma velha e forte 
+        <div class="flex gap-4 flex-col w-[200px] sm:w-[500px] h-full justify-center ml-[50px] sm:ml-[200px]">
+            <h1 class="text-white font-md text-[34px] sm:text-[44px] relative z-10">A melhor loja de Jordan</h1>
+            <p class="text-white font-md text-[18px] sm:text-[22px] relative z-10">O tênis Jordan é fruto de uma velha e forte 
                 parceria entre a Nike e o jogador Michael Jordan.
             </p>
         </div>
